f394bd8e94ff7d255197b95864ef6767c7678e89
davor.dimoski <davor.dimoski@hotmail.com>
2021-10-18 23:15:53 +0200

    init project

---
+++ src/test/kotlin/com/finki/emt/lostintranslation/LostInTranslationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.finki.emt.lostintranslation
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LostInTranslationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
